3c0c89d29af6c1f6f7336dff84ce9a6a154d1512
Jerrssey <fankton@gmail.com>
2023-03-29 18:03:13 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -26,7 +26,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['contacts']
+  whitelist: ['contacts'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
